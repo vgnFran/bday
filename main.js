9c1b5da6ebdc443d156dc03adb4b43de638f9a1c
vgnFran <francohoyos2112@gmail.com>
2023-03-04 19:25:39 -0300

    content

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const buttonFc = document.querySelector(".button-fc")
 const bgText= document.querySelector(".bg-text")
+const bgImg= document.querySelector("#bg-img")
 
 buttonFc.onclick=()=>{
     bgText.style.display="block"
     buttonFc.style.display="none"
+}
+
+bgImg.onclick=()=>{
+   bgImg.style.display="none"
 }
