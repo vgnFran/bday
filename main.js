e13135a31607cc81f6bfc1a768e17af45a69a251
vgnFran <francohoyos2112@gmail.com>
2023-03-04 02:18:30 -0300

    listo

---
+++ main.js
@@ -0,0 +1,7 @@
+const buttonFc = document.querySelector(".button-fc")
+const bgText= document.querySelector(".bg-text")
+
+buttonFc.onclick=()=>{
+    bgText.style.display="block"
+    buttonFc.style.display="none"
+}
