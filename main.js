957fb4b371a6515b909a32a1629056ad51b3e8f3
vgnFran <francohoyos2112@gmail.com>
2023-03-04 21:57:56 -0300

    fin

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const buttonFc = document.querySelector(".button-fc")
 const bgText= document.querySelector(".bg-text")
 const bgImg= document.querySelector("#bg-img")
+const travel= document.querySelector(".travel")
+const plane= document.querySelector(".core-page-loader")
+const bgFin= document.querySelector("#bg-img-fin")
+const fc= document.querySelector(".fc")
+const other=document.querySelector(".other")
+const another=document.querySelector(".another")
 
 buttonFc.onclick=()=>{
     bgText.style.display="block"
     buttonFc.style.display="none"
+    travel.style.display="block"
+    fc.style.display="none"
 }
 
-bgImg.onclick=()=>{
-   bgImg.style.display="none"
+travel.onclick=()=>{
+    plane.style.display="block"
+    setTimeout(()=>{
+        plane.style.display="none"
+        bgText.style.display="none"
+        bgImg.style.display="none"
+        travel.style.display="none"
+        bgFin.style.display="block"
+        other.style.display="block"
+        another.style.display="block"
+    },2000)
 }
